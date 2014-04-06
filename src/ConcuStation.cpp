30cf1dd27cc2395b890063c08876e41eeb5ee725
Agustín Mezzina <apmezzina@gmail.com>
2014-04-06 16:32:53 -0300

    Import inicial

---
+++ src/ConcuStation.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ConcuStation.cpp
+// Author      : 
+// Version     :
+// Copyright   :
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hola mundo" << endl;
+	return 0;
+}
